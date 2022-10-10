49b58f6e464c94eabaa08bad935ae360e93b9796
yamaishi <yamakios8@icloud.com>
2022-10-10 18:44:33 +0900

    投稿機能を作成

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,71 @@
+class PicturesController < ApplicationController
+  before_action :set_picture, only: %i[ show edit update destroy ]
+
+
+  def index
+    @pictures = Picture.all
+  end
+
+
+  def show
+  end
+
+
+  def new
+    @picture = Picture.new
+  end
+
+
+  def edit
+  end
+
+  def create
+    @picture = current_user.pictures.build(picture_params)
+
+    respond_to do |format|
+      if @picture.save
+        format.html { redirect_to picture_url(@picture), notice: "Picture was successfully created." }
+        format.json { render :show, status: :created, location: @picture }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @picture.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def confirm
+    @picture = Picture.new(picture_params)
+    @picture.user_id = current_user.id
+    render :new if @picture.invalid?
+  end
+
+  def update
+    respond_to do |format|
+      if @picture.update(picture_params)
+        format.html { redirect_to picture_url(@picture), notice: "Picture was successfully updated." }
+        format.json { render :show, status: :ok, location: @picture }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @picture.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @picture.destroy
+
+    respond_to do |format|
+      format.html { redirect_to pictures_url, notice: "Picture was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_picture
+      @picture = Picture.find(params[:id])
+    end
+
+    def picture_params
+      params.require(:picture).permit(:content, :image, :image_cache, :user_id)
+    end
+end
