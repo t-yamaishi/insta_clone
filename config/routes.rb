f6b0e7d50da1c2cd6d0b53cfb8bca483516b498f
yamaishi <yamakios8@icloud.com>
2022-10-11 09:16:09 +0900

    お気に入り一覧を作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   end
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, except: [:destroy]
-  resources :favorites, only: [:create, :destroy]
+  resources :favorites, only: [:index, :create, :destroy]
 end
